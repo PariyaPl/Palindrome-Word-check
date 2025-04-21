e63d3acf9f5fc70bb20444368166fccff3bab800
Pariya Palizban <131894233+PariyaPl@users.noreply.github.com>
2025-04-21 20:01:52 +0330

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+const textInput = document.querySelector("#text-input");
+const checkBtn = document.querySelector("#check-btn");
+const p = document.querySelector("#result");
+
+checkBtn.onclick = palindromeCheck;
+
+function palindromeCheck() {
+    const Word = textInput.value.toLowerCase().replace(/[\,\s\_\.\-]/g,'');
+    if(Word) {
+        const WordInv = Word.split('').reverse().join('').replace(/\\/g, '|').replace(/\//g, '\\').replace(/\|/g, '/').replace(/\(/g, '|').replace(/\)/g, '\(').replace(/\|/g, ')');
+        if(Word === WordInv) {
+            p.innerText = `${textInput.value} is a palindrome`;
+        }
+        else {
+            p.innerText = `${textInput.value} is not a palindrome`;
+        }
+    }
+    else{
+        alert("Please input a value");
+    }
+}
